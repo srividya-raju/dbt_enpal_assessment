cbe8f5f83b8912022fd4a588672ae885cef97841
srividya-raju <srividya.dec09@gmail.com>
2026-05-28 20:04:34 +0200

    Change order direction to ascending for funnel report

--- models/marts/rep_sales_funnel_monthly.sql
+++ models/marts/rep_sales_funnel_monthly.sql
@@ -5,7 +5,7 @@ select  date_trunc('month', deal_change_time)::date as month,
     coalesce(count(distinct deal_id), 0) as deal_count
 from {{ ref('int_union_deals_activity') }}
 group by date_trunc('month', deal_change_time)::date, funnel_step, kpi_name
-order by month, funnel_step
+order by month, funnel_step asc
 )
 
 select * from monthly_counts
